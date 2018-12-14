51b4dbcb40c688f5d5e1eb8068bc4a80417dc465
thibautbessone <thibaut13b@gmail.com>
2018-12-14 16:42:04 +0100

    almost working correctly i need to get the value out of the callback and done

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const fs = require('fs');
 const WordTrimmer = require('./lib/WordTrimmer');
 const WordTranslator = require('./lib/WordTranslator');
 
-const ideas = process.argv.splice(2); //gets args
-let dictionary = JSON.parse(fs.readFileSync('dictionary.json', 'utf8'));
+class Generator {
+    constructor(ideas) {
+        this.ideas = ideas;
+        this.names = "";
+    }
+    async generateNames() {
+        let generatorInstance = this;
+        let dictionary = JSON.parse(fs.readFileSync('dictionary.json', 'utf8'));
 
-let trimmer = new WordTrimmer(dictionary, ideas);
-let wordsToTranslate = trimmer.getEligibleWords();
+        let trimmer = new WordTrimmer(dictionary, generatorInstance.ideas);
+        let wordsToTranslate = trimmer.getEligibleWords();
 
-let translator = new WordTranslator(wordsToTranslate);
-translator.getTranslations().then(function () {
-    console.log(JSON.stringify(translator.translations));
-});
+        let translator = new WordTranslator(wordsToTranslate);
+        await translator.getTranslations().then(function () {
+            generatorInstance.names = JSON.stringify(translator.translations);
+            //FIXME return that ffs
+            console.log(generatorInstance.names);
+        });
+    }
+}
+
+module.exports = Generator;
 
 
